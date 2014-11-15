51d766c3455b9a80c3f00fb77eef426b7dc1ee76
aureooms <aurelien.ooms@gmail.com>
2014-11-15 22:23:01 +0100

    Generated documentation for 198d1b2b289c61651f8d12b58c716c2c8f044e9a

--- assets/behavior.js
+++ assets/behavior.js
@@ -62,25 +62,95 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       "depth": 1,
       "outline": []
     }, {
-      "type": "file",
+      "type": "folder",
       "data": {
-        "language": {
-          "nameMatchers": [".js"],
-          "pygmentsLexer": "javascript",
-          "multiLineComment": ["/*", "*", "*/"],
-          "singleLineComment": ["//"],
-          "ignorePrefix": "}",
-          "foldPrefix": "^",
-          "name": "JavaScript"
-        },
-        "sourcePath": "/home/aureooms/dev/js/js-compare/js/src/lexicographical.js",
-        "projectPath": "js/src/lexicographical.js",
-        "targetPath": "lexicographical",
-        "pageTitle": "lexicographical",
+        "path": "lexicographical",
         "title": "lexicographical"
       },
       "depth": 1,
-      "outline": []
+      "children": [
+        {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-compare/js/src/lexicographical/colexicographical.js",
+            "projectPath": "js/src/lexicographical/colexicographical.js",
+            "targetPath": "lexicographical/colexicographical",
+            "pageTitle": "lexicographical/colexicographical",
+            "title": "colexicographical"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-compare/js/src/lexicographical/lexicographical.js",
+            "projectPath": "js/src/lexicographical/lexicographical.js",
+            "targetPath": "lexicographical/lexicographical",
+            "pageTitle": "lexicographical/lexicographical",
+            "title": "lexicographical"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-compare/js/src/lexicographical/quasicolexicographical.js",
+            "projectPath": "js/src/lexicographical/quasicolexicographical.js",
+            "targetPath": "lexicographical/quasicolexicographical",
+            "pageTitle": "lexicographical/quasicolexicographical",
+            "title": "quasicolexicographical"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-compare/js/src/lexicographical/quasilexicographical.js",
+            "projectPath": "js/src/lexicographical/quasilexicographical.js",
+            "targetPath": "lexicographical/quasilexicographical",
+            "pageTitle": "lexicographical/quasilexicographical",
+            "title": "quasilexicographical"
+          },
+          "depth": 2,
+          "outline": []
+        }
+      ]
     }, {
       "type": "file",
       "data": {
