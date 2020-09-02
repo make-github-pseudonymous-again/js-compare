11cfa0a0c8f1af9e8148272970c0c5cca9134406
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-02 18:18:55 +0200

    :mag: test(native): Add more tests to pinpoint the exact spec.

--- test/src/decreasing.js
+++ test/src/decreasing.js
@@ -5,13 +5,25 @@ import * as compare from '../../src';
 test( "decreasing", t => {
 
 
-	t.truthy( compare.decreasing( Infinity,        0 )   < 0, "double <" );
-	t.truthy( compare.decreasing( Infinity, Infinity ) === 0, "double =" );
-	t.truthy( compare.decreasing(        0, Infinity )   > 0, "double >" );
+	t.true( compare.decreasing( Infinity,        0 )   < 0, "double <" );
+	t.true( compare.decreasing( Infinity, Infinity ) === 0, "double =" );
+	t.true( compare.decreasing(        0, Infinity )   > 0, "double >" );
 
-	t.truthy( compare.decreasing( "abc",  "ab" )   < 0, "string <" );
-	t.truthy( compare.decreasing( "abc", "abc" ) === 0, "string =" );
-	t.truthy( compare.decreasing(  "ab", "abc" )   > 0, "string >" );
+	t.true( compare.decreasing( "abc",  "ab" )   < 0, "string <" );
+	t.true( compare.decreasing( "abc", "abc" ) === 0, "string =" );
+	t.true( compare.decreasing(  "ab", "abc" )   > 0, "string >" );
+
+	t.true( compare.decreasing( NaN , NaN ) !== 0 , 'NaN != NaN');
+	t.true( compare.decreasing( NaN , 0 ) !== 0 , 'NaN != 0');
+	t.true( compare.decreasing( 0 , NaN ) !== 0 , '0 != NaN');
+	t.true( compare.decreasing( NaN , Infinity ) !== 0 , 'NaN != Infinity');
+	t.true( compare.decreasing( Infinity , NaN ) !== 0 , 'Infinity != NaN');
+	t.true( compare.decreasing( NaN , -Infinity ) !== 0 , 'NaN != -Infinity');
+	t.true( compare.decreasing( -Infinity , NaN ) !== 0 , '-Infinity != NaN');
+
+	t.true( compare.decreasing( new Date(1), new Date(1) ) !== 0, "this library is not suitable for date value equality checking" );
+	t.true( compare.decreasing( new Date(0), new Date(1) )   > 0, "date >" );
+	t.true( compare.decreasing( new Date(1), new Date(0) )   < 0, "date <" );
 
 
 });
--- test/src/increasing.js
+++ test/src/increasing.js
@@ -5,13 +5,24 @@ import * as compare from '../../src';
 test( "increasing", t => {
 
 
-	t.truthy( compare.increasing(        0, Infinity )   < 0, "double <" );
-	t.truthy( compare.increasing( Infinity, Infinity ) === 0, "double =" );
-	t.truthy( compare.increasing( Infinity,        0 )   > 0, "double >" );
+	t.true( compare.increasing(        0, Infinity )   < 0, "double <" );
+	t.true( compare.increasing( Infinity, Infinity ) === 0, "double =" );
+	t.true( compare.increasing( Infinity,        0 )   > 0, "double >" );
 
-	t.truthy( compare.increasing(  "ab", "abc" )   < 0, "string <" );
-	t.truthy( compare.increasing( "abc", "abc" ) === 0, "string =" );
-	t.truthy( compare.increasing( "abc",  "ab" )   > 0, "string >" );
+	t.true( compare.increasing(  "ab", "abc" )   < 0, "string <" );
+	t.true( compare.increasing( "abc", "abc" ) === 0, "string =" );
+	t.true( compare.increasing( "abc",  "ab" )   > 0, "string >" );
 
+	t.true( compare.increasing( NaN , NaN ) !== 0 , 'NaN != NaN');
+	t.true( compare.increasing( NaN , 0 ) !== 0 , 'NaN != 0');
+	t.true( compare.increasing( 0 , NaN ) !== 0 , '0 != NaN');
+	t.true( compare.increasing( NaN , Infinity ) !== 0 , 'NaN != Infinity');
+	t.true( compare.increasing( Infinity , NaN ) !== 0 , 'Infinity != NaN');
+	t.true( compare.increasing( NaN , -Infinity ) !== 0 , 'NaN != -Infinity');
+	t.true( compare.increasing( -Infinity , NaN ) !== 0 , '-Infinity != NaN');
+
+	t.true( compare.increasing( new Date(1), new Date(1) ) !== 0, "this library is not suitable for date value equality checking" );
+	t.true( compare.increasing( new Date(0), new Date(1) )   < 0, "date <" );
+	t.true( compare.increasing( new Date(1), new Date(0) )   > 0, "date >" );
 
 });
